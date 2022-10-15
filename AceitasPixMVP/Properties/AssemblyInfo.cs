de63ecb4d0a0d3b599bbaaf46299a25d470facbf
André Ferreira Pinto <servicos.andre@gmail.com>
2022-10-15 20:40:15 -0300

    troca para projeto com autenticação

--- AceitasPixMVP/Properties/AssemblyInfo.cs
+++ AceitasPixMVP/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9678badd-fdee-4d33-9ab3-1640627c5922")]
+[assembly: Guid("0264d086-a493-4e64-8300-5d5d13d3251f")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ AceitasPixMVP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AceitasPixMVP.Startup))]
+namespace AceitasPixMVP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
